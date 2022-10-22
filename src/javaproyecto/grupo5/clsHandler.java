86859ae38d401b5ebd1f864d099110c189a33f27
Joel <joel33960gmail.com>
2022-10-21 21:31:25 -0600

    clsHandler

--- src/javaproyecto/grupo5/clsHandler.java
+++ src/javaproyecto/grupo5/clsHandler.java
@@ -4,10 +4,23 @@
  */
 package javaproyecto.grupo5;
 
+import java.awt.TextArea;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author joel
  */
 public class clsHandler {
+    public void showMessage(String message) {
+        JOptionPane.showMessageDialog(null, message);
+    }
     
+    public void showMessage(TextArea message) {
+        JOptionPane.showMessageDialog(null, message);
+    }
+    
+    public char inputChar(String mensaje){
+        return JOptionPane.showInputDialog(mensaje).toLowerCase().charAt(0);
+    }
 }
